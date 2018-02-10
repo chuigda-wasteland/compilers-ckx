5f52b9a703ddee9aabf10f60057e852dfb7603bc
Shirou Kotomine <crazycz.cc@ckx-ily.cn>
2018-02-11 01:09:23 +0800

    [(UTC +8:00) 2018-2-11 1:09] ckx command line tool experimental

--- src/driver/main.cpp
+++ src/driver/main.cpp
@@ -1,4 +1,33 @@
-﻿int main()
+﻿#include "frontend/lex/ckx_token_stream.hpp"
+#include "frontend/parse/ckx_parser.hpp"
+#include "frontend/sema/ckx_sema.hpp"
+
+int main(int argc, char** argv)
 {
+    using namespace ckx;
+    using namespace we;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ckxc filename\n");
+        return -1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == nullptr)
+    {
+        fprintf(stderr, "File %s not found\n", argv[1]);
+        return -2;
+    }
+
+    we_fp_reader reader(fp);
+    ckx_token_stream stream(reader);
+    ckx_sema_engine sema;
+
+    ckx_parser().parse(&stream).trans_unit->accept(sema);
+    we_fp_writer writer(stdout);
+    sema.test_print(writer);
+    fclose(fp);
+
     return 0;
 }
